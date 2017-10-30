30fc5d15168f808f382fc4f3935616991f8dc94a
马柏樟 <mabz@chanjet.com>
2017-10-30 22:19:06 +0800

    upload program

--- tools/src/main/java/LeetCode/DegreeofanArray.java
+++ tools/src/main/java/LeetCode/DegreeofanArray.java
@@ -36,6 +36,7 @@ public class DegreeofanArray {
     public static void main(String[] args) {
         int[] x = {1, 2, 2, 1, 2, 1, 1, 1, 1, 2, 2, 2};
         System.out.println(findShortestSubArray(x));
+        System.out.println('A'-64);
     }
 
 }
